b0806bdfa71ddbfc87eea371ad4322533a98f2ec
Vijenthiny Jiththendiran <vijenthinyjithth@gmail.com>
2023-08-06 14:38:12 -0400

    new file created

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+app.use(require('./routes'));
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
